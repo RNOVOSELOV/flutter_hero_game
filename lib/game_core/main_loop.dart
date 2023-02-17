dc2a74550389c4f5b980dd41bb66d6901a448499
RNOVOSELOV <novoselov_roman@mail.ru>
2023-02-17 13:37:25 +0300

    Landskape mode

---
+++ lib/game_core/main_loop.dart
@@ -0,0 +1,31 @@
+bool _running = true;
+
+void startLoop() {
+  const fps = 50;
+  const second = 1000;
+  const updateTime = second / fps;
+  int updates = 0;
+
+  Stopwatch loopWatch = Stopwatch();
+  loopWatch.start();
+  Stopwatch timerWatch = Stopwatch();
+  timerWatch.start();
+
+  while (_running) {
+    if (loopWatch.elapsedMilliseconds > updateTime) {
+      updates++;
+      loopWatch.reset();
+    }
+
+    if (timerWatch.elapsedMilliseconds > second) {
+      print("${DateTime.now()} FPS: $updates");
+      updates = 0;
+      timerWatch.reset();
+    }
+
+  }
+}
+
+void stopLoop () {
+  _running = false;
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:spacehero/game_core/main_loop.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).whenComplete(() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom],
+    );
+    runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Game"),
+      ),
+    ));
+  });
+}
